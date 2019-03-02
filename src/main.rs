59c3de535ed02ae7a3077f3715e1f3787928e34e
YoshiyukiKato <yo.shuuuuki@gmail.com>
2019-03-03 05:41:18 +0900

    guessing game with ferris

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,46 @@
 use ferris_says::say;
 use std::io::{stdin, stdout, BufWriter};
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    let stdin = stdin();
-    let mut message = String::new();
-    stdin.read_line(&mut message).expect("Failed to readline");
-    let stdout = stdout();
-    let width = 24;
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    let mut ferris = Ferris::new(24);
+    ferris.ask();
+}
+
+struct Ferris {
+    number: i32,
+    width: usize
+}
+
+impl Ferris {
+    fn new(width:usize) -> Ferris {
+        Ferris {
+            width: width,
+            number: rand::thread_rng().gen_range(1, 11)
+        }
+    }
+
+    fn say(&mut self, message: &str) {
+        let mut writer = BufWriter::new(stdout());
+        say(message.as_bytes(), self.width, &mut writer).unwrap();
+    }
+    
+    pub fn ask(&mut self){
+        self.say("Guess a number in 1-10");
+        let stdin = stdin();
+        loop {
+            let mut input = String::new();
+            stdin.read_line(&mut input).expect("Failed to readline");
+            let guess: i32 = input.trim().parse().expect("Please type a number!");
+            match guess.cmp(&self.number) {
+                Ordering::Less => self.say("Too small!"),
+                Ordering::Greater => self.say("Too big!"),
+                Ordering::Equal => {
+                    self.say("You win!");
+                    break;
+                }
+            }
+        }
+    }
 }
